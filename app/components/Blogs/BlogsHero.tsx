b6e3660ada8249062eb64df30b4876642f286d98
ali.esmaeel@visticle.de <ali.esmaeel@visticle.de>
2025-12-05 01:31:03 +0000

    fix  news page hero and blogs hero on moblie

--- app/components/Blogs/BlogsHero.tsx
+++ app/components/Blogs/BlogsHero.tsx
@@ -3,16 +3,16 @@ import { useLoaderData } from "react-router";
 
 export default function BlogsHero() {
   const { blog } = useLoaderData() as { blog: any };
-
+  console.log(blog);
   return (
-    <div className="flex flex-col items-center justify-center w-full h-screen relative">
+    <div className="flex flex-col items-center justify-center w-full h-[48vh] lg:h-screen relative">
       <img
         loading="lazy"
-        src={blog.blog_image.url}
+        src={blog.blog_image[0].url}
         alt=""
-        className="w-full h-screen object-cover"
+        className="w-full h-screen object-contain lg:object-cover"
       />
-      <div className="flex flex-col items-center justify-center w-full h-screen absolute top-0 left-0 px-[16px] lg:px-[45px]">
+      <div className="flex flex-col items-center justify-center w-full h-[50vh] lg:h-screen absolute top-0 left-0 px-[16px] lg:px-[45px]">
         <div className="flex flex-col items-center gap-[53.68px] w-full max-w-[1226px]">
           <div className="flex flex-col items-center gap-[17px] w-full">
             <h1 className="text-white text-[16px] lg:text-[51.04px] text-center ">{blog.title}</h1>
