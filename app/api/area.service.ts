44313f65216c8f9d3991cbedaed49623cb056e99
ali.esmaeel@visticle.de <ali.esmaeel@visticle.de>
2026-01-04 23:37:48 +0000

    add charts to realestate

--- app/api/area.service.ts
+++ app/api/area.service.ts
@@ -5,3 +5,7 @@ const api = new ApiClient();
 export async function getArea(areaSlug: string) {
   return api.get(`/api/popular-areas/${areaSlug}`);
 }
+
+export async function getAreaTransactions(areaName: string) {
+  return api.get(`/api/area-transactions?area_name=${encodeURIComponent(areaName)}`);
+}
