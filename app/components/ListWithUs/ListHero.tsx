e721f2b7de0f16cd37e02a11d20de28c809a8b37
ali.esmaeel@visticle.de <ali.esmaeel@visticle.de>
2026-02-19 23:39:50 +0000

    fix design in list with us page

--- app/components/ListWithUs/ListHero.tsx
+++ app/components/ListWithUs/ListHero.tsx
@@ -2,14 +2,14 @@ import React from "react";
 
 export default function ListHero() {
   return (
-    <div className="flex flex-col items-center justify-center w-full h-[80vh] lg:h-[150vh] relative">
+    <div className="flex flex-col items-center justify-center w-full h-[80vh] lg:h-[110vh] relative">
       <img
         loading="lazy"
         src="/images/icons/vLetter.png"
         alt=""
         className="w-full h-[80vh] lg:h-[150vh] object-cover"
       />
-      <div className="flex flex-col items-center justify-center w-full h-[80vh] lg:h-[150vh] absolute top-0 left-0 px-[16px] lg:px-[45px]">
+      <div className="flex flex-col items-center justify-end w-full h-[80vh] lg:h-[110vh] absolute top-0 left-0 px-[16px] lg:px-[45px]">
         <div className="flex flex-col items-center gap-[0px] w-full">
           <div className="flex flex-col items-center gap-[17px]">
             <h1 className=" text-[20px] lg:text-[45px] font-semibold">
