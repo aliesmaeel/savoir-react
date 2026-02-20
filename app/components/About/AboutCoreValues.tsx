ba23d03992d9a0a634042538cdd751dc17a1f6a2
ali.esmaeel@visticle.de <ali.esmaeel@visticle.de>
2026-02-20 20:30:46 +0000

    fix images in about us page

--- app/components/About/AboutCoreValues.tsx
+++ app/components/About/AboutCoreValues.tsx
@@ -8,22 +8,22 @@ export default function AboutCoreValues() {
     {
       title: "Knowledge",
       text: "In our real estate domain, knowledge is not just information; it is  power. At Savoir Privé Properties, we empower our clients with the  information they need to make the smartest choices, recognizing that  knowledge is the key to success in the dynamic world of real estate.",
-      image: "/images/about/1.jpg",
+      image: "/images/about/Knowledge.jpg",
     },
     {
       title: "Passion",
       text: "We firmly believe that genuine passion is the driving force behind  transformative change. At Savoir Privé Properties, we wholeheartedly  commit ourselves to creating a positive influence in the lives of our  clients. Our motivation is humble yet powerful – to change our clients'  world for the better through unwavering passion.",
-      image: "/images/about/2.jpg",
+      image: "/images/about/Passion.jpg",
     },
     {
       title: "Trust",
       text: "At SAVOIR we do not have clients, we have partners. This is a healthy relationship based on trust and mutual understanding. Trust is what leads us to success.",
-      image: "/images/about/4.jpg",
+      image: "/images/about/Trust.jpg",
     },
     {
       title: "Excellence",
       text: "The synergy of knowledge and  experience culminates in excellence, and that is precisely what we stand for at Savoir Privé Properties. Our commitment is to provide excellence in every facet of our work. As wisdom and hands-on expertise converge, a new echelon of quality defines our distinctive approach to the realm of real estate.",
-      image: "/images/about/4.JPG",
+      image: "/images/about/Excellence.jpg",
     },
   ];
 
